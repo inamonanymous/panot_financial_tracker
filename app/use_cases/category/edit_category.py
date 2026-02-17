b27be504b8e19417265cd4fc1386a9d3b3f179f9
Stephen Aguilar <stephenonline25@gmail.com>
2026-02-17 21:44:20 +0800

    Add Category feature is released

---
+++ app/use_cases/category/edit_category.py
@@ -0,0 +1,20 @@
+from app.domain.policies.p_CategoryPolicy import CategoryPolicy
+from app.utils.exceptions import PolicyError
+
+class EditCategoryUseCase:
+    def __init__(self, unit_of_work):
+        self.uow = unit_of_work
+        self.cat_policy = CategoryPolicy()
+
+    def execute(self, category_data: dict):
+        category = self.uow.categories.get_by_id_and_user_id(category_data["category_id"], category_data["user_id"])
+        clean_data = self.cat_policy.validate_category_editing(category_data, category)
+        duplicate_category = self.uow.categories.get_by_name_and_user_id(clean_data["name"], clean_data["user_id"])
+        self.cat_policy.validate_duplicate_category_name_entry(duplicate_category)
+
+        category.rename(clean_data["name"])
+
+        with self.uow.transaction():
+            updated_category = self.uow.categories.update(category)
+
+        return updated_category
